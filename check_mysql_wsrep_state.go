74247f1eb021ff4d7ca4c34d6aaf44fa00c0feb3
chrispetsos <cpetsos@pccwglobal.com>
2019-01-31 13:48:46 +0000

    Using third and fourth arguments as MySQL user/pass.

--- check_mysql_wsrep_state.go
+++ check_mysql_wsrep_state.go
@@ -3,16 +3,37 @@ package main
 import "database/sql"
 import _ "github.com/go-sql-driver/mysql"
 import "os"
+// import "fmt"
 
 var db *sql.DB
 
 func main() {
   mysql_server_host := os.Args[1]
   mysql_server_port := os.Args[2]
+  mysql_server_user := ""
+  if len(os.Args) > 3 {
+    mysql_server_user = os.Args[3]
+  }
+  mysql_server_pass := ""
+  if len(os.Args) > 4 {
+    mysql_server_pass = os.Args[4]
+  }
 
   var err error
 
-  db, err = sql.Open("mysql", "haproxy_check@tcp(" + mysql_server_host + ":" + mysql_server_port + ")/")
+  mysql_url := ""
+  if mysql_server_user != "" {
+    mysql_url += mysql_server_user
+  }
+  if mysql_server_pass != "" {
+    mysql_url += ":" + mysql_server_pass
+  }
+  if mysql_server_user != "" {
+    mysql_url += "@"
+  }
+  mysql_url += "tcp(" + mysql_server_host + ":" + mysql_server_port + ")/"
+
+  db, err = sql.Open("mysql", mysql_url)
   if err != nil {
     panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
   }
